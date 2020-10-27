d9e39960a2a93ec09de5d467f26108efc404ea1b
Valerii Karimov <valeraguess@gmail.com>
2020-10-27 09:10:17 +0200

    add new modules

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -1,9 +1,8 @@
-export default class Slider {
-  constructor(page, btns) {
-    this.page = document.querySelector(page);
-    this.slides = this.page.children;
-    this.btns = document.querySelectorAll(btns);
-    this.slideIndex = 1;
+import Slider from './slider';
+
+export default class MainSlider extends Slider {
+  constructor(btns) {
+    super(btns);
   }
 
   showSlides(n) {
@@ -12,7 +11,7 @@ export default class Slider {
     }
 
     if (n < 1) {
-      this.slideIndex = slides.length;
+      this.slideIndex = this.slides.length;
     }
 
     try {
@@ -40,11 +39,7 @@ export default class Slider {
     this.showSlides(this.slideIndex += n);
   }
 
-  render() {
-    try {
-      this.hanson = document.querySelector('.hanson');
-    } catch(e){}
-
+  bindTriggers() {
     this.btns.forEach(item => {
       item.addEventListener('click', () => {
         this.plusSlides(1);
@@ -56,5 +51,34 @@ export default class Slider {
         this.showSlides(this.slideIndex);
       });
     });
+
+    // this.showSlides(this.slideIndex);
+
+    document.querySelectorAll('.prevmodule').forEach(item => {
+      item.addEventListener('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        this.plusSlides(-1);
+      });
+    });
+
+    document.querySelectorAll('.nextmodule').forEach(item => {
+      item.addEventListener('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        this.plusSlides(1);
+      });
+    });
+  }
+
+  render() {
+    if (this.container) {
+      try {
+        this.hanson = document.querySelector('.hanson');
+      } catch(e){}
+  
+      this.showSlides(this.slideIndex);
+      this.bindTriggers();
+    } 
   }
 }
